17972f1828b79e5b7f95c8953d5ef62a74595bfb
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-16 09:26:17 +0300

    update

--- homework/classes/Generic.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Generic {
-    public <E> void printArray(E[] data) {
-        for (E item: data) {
-            System.out.print("[ " + item + " ]");
-        }
-    }
-}
