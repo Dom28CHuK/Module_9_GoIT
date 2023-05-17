97e057ea070c75ed351e8f78db0c0f9480eaa671
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-17 13:12:20 +0300

    add class MyArrayList

--- homework/Main.java
+++ homework/Main.java
@@ -1,5 +1,17 @@
+import classes.MyArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        MyArrayList myArrayList = new MyArrayList();
+
+        myArrayList.add("Val");
+        myArrayList.add("And");
+        myArrayList.add("Hom");
+        System.out.println(myArrayList);
+
+
+        myArrayList.get(2);
+        myArrayList.size();
 
     }
 }
--- homework/classes/MyArrayList.java
+++ homework/classes/MyArrayList.java
@@ -1,24 +1,52 @@
 package classes;
 
+import java.util.Arrays;
+
 public class MyArrayList {
 
     private Object[] objects;
     private int size;
 
-    public void add(Object value) {
+    public MyArrayList() {
+        this.objects = new Object[1];
+        this.size = 0;
+    }
 
+    public void add(Object value) {
+        if (size == objects.length) {
+            Object[] newObjects = new Object[objects.length * 2];
+            System.arraycopy(objects, 0, newObjects, 0, size);
+            objects = newObjects;
+        }
+        objects[size] = value;
+        size++;
     }
 
     public void remove(int index) {
-
+        for (int i = index; i < size - 1; i++) {
+            objects[i] = objects[i + 1];
+        }
+        objects[size - 1] = null;
+        size--;
     }
     public void clear() {
-
+        size = 0;
+        for (int i = 0; i < objects.length; i++) {
+            objects[i] = null;
+        }
     }
     public int size() {
+        System.out.println("Array size [ " + size + " ]");
         return size;
     }
-    public void get(int index) {
+    public Object get(int index) {
+        System.out.println("Element by index [ " + objects[index] + " ]");
+        return objects[index];
+    }
 
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "objects=" + Arrays.toString(objects);
     }
 }
