564e8efeb47f9c296313f0c73470fa675e67491f
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-22 06:53:51 +0300

    MyHashMap

---
+++ homework/classes/MyHashMap.java
@@ -0,0 +1,94 @@
+package classes;
+
+public class MyHashMap {
+    private Node[] table;
+    private int size;
+
+    private static class Node {
+        private Object key;
+        private Object value;
+        private Node next;
+
+        public Node(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public MyHashMap() {
+        table = new Node[16];
+        size = 0;
+    }
+
+    public void put(Object key, Object value) {
+        int index = getIndex(key);
+        Node newNode = new Node(key, value);
+
+        if (table[index] == null) {
+            table[index] = newNode;
+        } else {
+            Node current = table[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
+            }
+            if (current.key.equals(key)) {
+                current.value = value;
+            } else {
+                current.next = newNode;
+            }
+        }
+        size++;
+    }
+
+    public void remove(Object key) {
+        int index = getIndex(key);
+        Node current = table[index];
+        Node prev = null;
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (prev == null) {
+                    table[index] = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                size--;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+    }
+
+    public void clear() {
+        table = new Node[16];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(Object key) {
+        int index = getIndex(key);
+        Node current = table[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    private int getIndex(Object key) {
+        return Math.abs(key.hashCode()) % table.length;
+    }
+}
+
